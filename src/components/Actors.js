6134f78524dfbe5642e37633fe9b763685f6155b
Ben Spotswood <spotswoodb@gmail.com>
2021-11-12 16:19:10 -0600

    commit tree c0feca1c135c496d794313d361f2a8e586d56207

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+
+  const actorsDiv = actors.map((a, i) => (
+    <div key={i}>
+      <h3>Name: {a.name}</h3>
+      <ul>
+        {a.movies.map((m, i) => (
+          <li key={i}>{m}</li>
+        ))}
+
+      </ul>
+    </div>
+  ))
+
+
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        {actorsDiv}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const directorsDiv = directors.map((d, i) => (
+    <div key={i}>
+      <h3>Name {d.name}</h3>
+      <ul>
+        {d.movies.map((m,i) => (
+          <li key={i}>{m}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsDiv}
     </div>
   );
 }
